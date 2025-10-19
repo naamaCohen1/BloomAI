6ac5bdc58729fa99d5bef4e26ae1c76a2c947acb
naamaCohen2 <naama.cohen@litepoint.com>
2025-10-19 22:50:08 +0300

    upload files for start

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (_req, res) => res.send("BloomAI is alive 🌿"));
+
+app.listen(3000, () =>
+    console.log(`Server running on http://localhost:${3000}`)
+);
+
